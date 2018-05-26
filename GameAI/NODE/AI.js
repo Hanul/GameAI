e0153c8fae9d12c340fb6570da7115b1271e6a41
Hanul <hanul@hanul.me>
2018-05-26 14:59:53 +0900

    no message

---
+++ GameAI/NODE/AI.js
@@ -0,0 +1,16 @@
+/*
+ * 온라인 게임에서 사용되는 인공지능
+ */
+GameAI.AI = CLASS({
+	
+	init : (inner, self, params) => {
+		//REQUIRED: params
+		//REQUIRED: params.host	소켓 서버 호스트
+		//REQUIRED: params.port	소켓 서버 포트
+		
+		// 인공지능 삭제
+		let remove = self.remove = () => {
+			
+		};
+	}
+});
--- GameAISampleServer/NODE/MAIN.js
+++ GameAISampleServer/NODE/MAIN.js
@@ -2,7 +2,5 @@ GameAISampleServer.MAIN = METHOD({
 
 	run : () => {
 		
-		// 릴레이 서버 실행
-		SkyRelay.Server(CONFIG.relayServerPort);
 	}
 });
