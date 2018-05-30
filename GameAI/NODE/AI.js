62cf5bc6f69b24ffda71da3b19359cb7cdbc1769
YJ <hanul@hanul.me>
2018-05-31 02:55:40 +0900

    인공지능 대전 기능 구현

--- GameAI/NODE/AI.js
+++ GameAI/NODE/AI.js
@@ -5,37 +5,96 @@ GameAI.AI = CLASS({
 	
 	init : (inner, self, params, handlers) => {
 		//REQUIRED: params
+		//REQUIRED: params.x
+		//REQUIRED: params.y
+		//REQUIRED: params.minMovableX
+		//REQUIRED: params.maxMovableX
+		//REQUIRED: params.minMovableY
+		//REQUIRED: params.maxMovableY
 		//REQUIRED: params.genre
 		//REQUIRED: params.aggression
+		//REQUIRED: params.accuracy
 		//REQUIRED: handlers
-		//OPTIONAL: handlers.findTarget
+		//OPTIONAL: handlers.findTargetPosition
 		//OPTIONAL: handlers.moveTo
 		//OPTIONAL: handlers.shoot
 		
+		let x = params.x;
+		let y = params.y;
+		let minMovableX = params.minMovableX;
+		let maxMovableX = params.maxMovableX;
+		let minMovableY = params.minMovableY;
+		let maxMovableY = params.maxMovableY;
+		let aggression = params.aggression;
+		
+		let findTargetPosition = handlers.findTargetPosition;
 		let moveTo = handlers.moveTo;
 		let shoot = handlers.shoot;
 		
-		let id = UUID();
+		// 장르가 탑뷰 FPS인 경우, 총알을 피하며 유저가 이동하려 하는 곳에 탄을 발사합니다.
 		
-		let moveInterval = INTERVAL(1, () => {
-			moveTo( RANDOM({
-				min : 0,
-				max : 1280
-			}),  RANDOM({
-				min : 0,
-				max : 720
-			}));
-			
-			shoot();
-		});
+		let id = UUID();
 		
 		let getId = self.getId = () => {
 			return id;
 		};
 		
+		let toX = x;
+		let toY = y;
+		
+		let move = () => {
+			
+			toX = RANDOM({
+				min : minMovableX,
+				max : maxMovableX
+			});
+			toY = RANDOM({
+				min : minMovableY,
+				max : maxMovableY
+			});
+			
+			moveTo(toX, toY);
+		};
+		
+		let moveDelay;
+		let createMoveDelay = RAR(() => {
+			
+			moveDelay = DELAY(RANDOM({
+				min : 10,
+				max : 200
+			}) / 100, () => {
+				
+				move();
+				
+				createMoveDelay();
+			});
+		});
+		
+		let shootDelay;
+		let createShootDelay = RAR(() => {
+			
+			shootDelay = DELAY(RANDOM({
+				min : 5,
+				max : 50
+			}) / 100, () => {
+				
+				let targetPosition = findTargetPosition();
+				
+				shoot(Math.atan2(toX - targetPosition.x, targetPosition.y - toY));
+				
+				createShootDelay();
+			});
+		});
+		
+		// 상대가 공격하는 것을 감지하고 이동
+		let announceTargetShoot = self.announceTargetShoot = () => {
+			move();
+		};
+		
 		// 인공지능 제거
 		let remove = self.remove = () => {
-			
+			moveDelay.remove();
+			shootDelay.remove();
 		};
 	}
 });
--- GameAISampleServer/NODE/GameRoom.js
+++ GameAISampleServer/NODE/GameRoom.js
@@ -13,15 +13,35 @@ GameAISampleServer.GameRoom = OBJECT({
 				max : 720
 			});
 			
+			let unitMoveSpeed = 600;
+			
+			let lastHeroMoveInfo = {
+				fromX : 0,
+				fromY : 0,
+				toX : 0,
+				toY : 0,
+				time : Date.now()
+			};
+			
 			// 누군가가 접속하면, 해당 유저와 상대할 AI를 생성합니다.
 			let enemyAI = GameAI.AI({
+				
 				x : enemyX,
 				y : enemyY,
+				minMovableX : 0,
+				maxMovableX : 1280,
+				minMovableY : 0,
+				maxMovableY : 720,
+				
 				genre : 'topview-fps',
-				aggression : 100
+				aggression : 100,
+				accuracy : 100
 			}, {
-				findTarget : () => {
-					
+				findTargetPosition : () => {
+					return {
+						x : lastHeroMoveInfo.toX,
+						y : lastHeroMoveInfo.toY
+					};
 				},
 				moveTo : (x, y) => {
 					send({
@@ -33,22 +53,44 @@ GameAISampleServer.GameRoom = OBJECT({
 						}
 					});
 				},
-				shoot : () => {
+				shoot : (angle) => {
 					send({
 						methodName : 'shootEnemy',
 						data : {
-							id : enemyAI.getId()
+							id : enemyAI.getId(),
+							angle : angle
 						}
 					});
 				}
 			});
 			
+			// AI를 출현시킵니다.
 			send({
 				methodName : 'showEnemy',
 				data : {
 					id : enemyAI.getId(),
 					x : enemyX,
 					y : enemyY
+				}
+			});
+			
+			// 유저가 이동할 때
+			on('moveHero', (params) => {
+				if (params !== undefined) {
+					lastHeroMoveInfo.fromX = params.fromX;
+					lastHeroMoveInfo.fromY = params.fromY;
+					lastHeroMoveInfo.toX = params.toX;
+					lastHeroMoveInfo.toY = params.toY;
+					lastHeroMoveInfo.time = Date.now();
+				}
+			});
+			
+			// 유저가 공격할 때
+			on('shootHero', (params) => {
+				if (params !== undefined) {
+					
+					// AI에게 알립니다.
+					enemyAI.announceTargetShoot();
 				}
 			});
 			
